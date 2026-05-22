536103c974a04747bfa096c178b1a71476392c52
aadyasingh2 <aadya2630@gmail.com>
2026-05-23 02:35:00 +0530

    fetched the contributionData from GraphQL API

---
+++ FetchContributions.py
@@ -0,0 +1,37 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+
+token= os.getenv("GITHUB_TOKEN")
+URL="https://api.github.com/graphql"
+username="aadyasingh2"
+query=f"""query{{
+            user(login:"{username}"){{
+                contributionsCollection
+                {{
+                    contributionCalendar
+                  {{
+                        totalContributions,
+                        weeks
+                        {{
+                            contributionDays
+                            {{
+                                date,contributionCount
+                            }}
+                        }}
+                    }}
+                }}
+            }}
+        }}"""
+response=requests.post(URL,json={"query":query},headers={'Authorization':"Bearer "+token})
+print(response.json())
+graphdata=response.json()['data']['user']['contributionsCollection']['contributionCalendar']['weeks']
+
+for week in graphdata:
+    weeklyContributions=week['contributionDays']
+    for day in weeklyContributions:
+        print(f"Today's date: {day['date']}\n Contributions Made Today: {day['contributionCount']}")
